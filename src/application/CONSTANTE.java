668b701295b0b74b70d56a57ea513c0011dff85a
Jeremy Portelli <jeremyp.1994@hotmail.com>
2016-04-21 20:36:04 +0200

    MineHunt

---
+++ src/application/CONSTANTE.java
@@ -0,0 +1,11 @@
+package application;
+
+public final class CONSTANTE {
+	public static final int NBR_ROW = 4;
+	public static final int NBR_COL = 4;
+	public static final int NBR_MINES = 2;
+	public static final String TITLE = "application/resources/minehunt.png";
+	public static final String MINE = "application/resources/mine.png";
+	public static final String FLAG = "application/resources/flag.png";
+
+}
---
+++ src/application/CellButton.java
@@ -0,0 +1,62 @@
+package application;
+
+import javafx.scene.control.Button;
+
+public class CellButton extends Button {
+
+  private boolean flag;
+  private boolean mine;
+  private boolean open;
+  private int     row;
+  private int     col;
+
+  public CellButton(int row, int col) {
+    flag = false;
+    open = false;
+    mine = false;
+    this.row = row;
+    this.col = col;
+  }
+
+  public boolean isFlag() {
+    return flag;
+  }
+
+  public void setFlag(boolean flag) {
+    this.flag = flag;
+  }
+
+  public boolean isMine() {
+    return mine;
+  }
+
+  public void setMine(boolean mine) {
+    this.mine = mine;
+  }
+
+  public boolean isOpen() {
+    return open;
+  }
+
+  public void setOpen(boolean open) {
+    this.open = open;
+  }
+
+  public int getRow() {
+    return row;
+  }
+
+  public void setRow(int row) {
+    this.row = row;
+  }
+
+  public int getCol() {
+    return col;
+  }
+
+  public void setCol(int col) {
+    this.col = col;
+  }
+  
+
+}
